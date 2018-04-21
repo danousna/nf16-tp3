012d09449c7e1f82c8904012f74d3562935e1b71
youtous <contact@youtous.me>
2018-04-21 17:53:54 +0200

    ajout structure tests

---
+++ tests.c
@@ -0,0 +1,81 @@
+//
+// Created by youtous on 21/04/18.
+//
+
+#include "tests.h"
+
+void initTests() {
+
+}
+
+int testAjoutPremiereTransaction() {
+    return 0;
+}
+
+int testAjoutAutreTransactions() {
+    return 0;
+}
+
+int testBlockPremierId() {
+    return 0;
+}
+
+int testBlockSuivantId() {
+    return 0;
+}
+
+int testBlockTransactionVide() {
+    return 0;
+}
+
+int testSommeCoinsJourneeEtudiantPasDeTransaction() {
+    return 0;
+}
+
+int testSommeCoinsJourneeEtudiant() {
+    return 0;
+}
+
+int testCalculSoldeTotalNul() {
+    return 0;
+}
+
+int testCalculSoldeTotalNonNul() {
+    return 0;
+}
+
+int testCreditCompteVerifTransactionTeteBlock() {
+    return 0;
+}
+
+int testPayerRepasPasAssez() {
+    return 0;
+}
+
+int testPayerRepasAssezArgentNouvelleTransaction() {
+    return 0;
+}
+
+int testHistoriqueMoins5Transactions() {
+    return 0;
+}
+
+int testHistoriqueAucuneTransaction() {
+    return 0;
+}
+
+int testHistoriquePlus5HistoriquesChronologique() {
+    return 0;
+}
+
+int testTransfertPasAssezArgentOrgine() {
+    return 0;
+}
+
+int testTransfertReussi2TransactionsAttendues() {
+    return 0;
+}
+
+int testImportExportImmuables() {
+    return 0;
+}
---
+++ tests.h
@@ -0,0 +1,37 @@
+//
+// Created by youtous on 21/04/18.
+//
+
+#ifndef NF16_TP3_TESTS_H
+#define NF16_TP3_TESTS_H
+
+void initTests();
+
+int testAjoutPremiereTransaction();
+int testAjoutAutreTransactions();
+
+int testBlockPremierId();
+int testBlockSuivantId();
+int testBlockTransactionVide();
+
+int testSommeCoinsJourneeEtudiantPasDeTransaction();
+int testSommeCoinsJourneeEtudiant();
+
+int testCalculSoldeTotalNul();
+int testCalculSoldeTotalNonNul();
+
+int testCreditCompteVerifTransactionTeteBlock();
+
+int testPayerRepasPasAssez();
+int testPayerRepasAssezArgentNouvelleTransaction();
+
+int testHistoriqueMoins5Transactions();
+int testHistoriqueAucuneTransaction();
+int testHistoriquePlus5HistoriquesChronologique();
+
+int testTransfertPasAssezArgentOrgine();
+int testTransfertReussi2TransactionsAttendues();
+
+int testImportExportImmuables();
+
+#endif //NF16_TP3_TESTS_H
